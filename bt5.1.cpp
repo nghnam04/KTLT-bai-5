65a1336c1c0aa262945400f7fad935dd2a1f84f1
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-06-13 18:56:24 +0700

    Bài 1 sửa lỗi Syntax in ra hoán vịn số

--- bt5.1.cpp
+++ bt5.1.cpp
@@ -4,26 +4,30 @@ MSSV: 20225213
 Ma lop: 738920
 */
 #include <stdio.h>
+
 int x[100], mark[100], n;
+
 void print(){
-for (int i = 1; i <= n; ++i) printf("%d ", x[i]);
-printf("\n");
+    for (int i = 1; i <= n; ++i) printf("%d ", x[i]);
+    printf("\n");
 }
+
 void process(int i) {
-if (i > n){
-print();
-return;
+    if (i > n){
+        print();
+        return;
+    }
+    for (int j = 1; j <= n; ++j)
+        if (!mark[j]){
+            mark[j] = 1;
+            x[i] = j;
+            process(i+1);
+            mark[j] = 0;
+        }
 }
-for (int j = 1; j <= n; ++j)
-if (!mark[j]){
-mark[j] = 1;
-x[i] = j;
-process(i+1);
-mark[j] = 0;
-}
-}
+
 int main() {
-n = 5;
-process(1);
-return 0;
+    n = 5;
+    process(1);
+    return 0;
 }
